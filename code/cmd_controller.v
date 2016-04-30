d99856e4bf88b8bffe1f4c052dc1f1a0bc22fed6
erickeduarte <erickedur@gmail.com>
2016-04-30 17:48:07 -0600

    inputs-outputs cmd controller

--- code/cmd_controller.v
+++ code/cmd_controller.v
@@ -1,10 +1,25 @@
 module cmd_controller(
 
-
-
-
-
-
+	// Inputs from Host Interface
+	input clock,
+	input reset,
+	
+	input data_read,
+	input data_write,
+	
+	input argument_reg,
+	
+	
+	// Inputs and Outputs from Serial Host
+	input ack_in,
+	input req_in,
+	input cmd_in,
+	
+	output ack_out,
+	output req_oout,
+	output cmd_out,
+	
+	output idle_out,
 
 
 
