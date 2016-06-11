f58d5e177631d789e534a438df22380ac372e0f4
Diego Soto <ludisu13@gmail.com>
2016-06-10 20:46:56 -0600

    rearquitecturizacion cmd physical comleta

--- code/cmd_phys.v
+++ code/cmd_phys.v
@@ -5,6 +5,7 @@
 `include "serialtoparallelwrapper.v"
 `include "serialToParallel.v"
 `include "parallelToSerial.v"
+`include "cmd_phys_controller.v"
 module cmd_phys(
 	input wire sd_clock,
 	input wire reset,
@@ -14,9 +15,9 @@ module cmd_phys(
 	input wire idle_in, // sets as idle
 	input wire [39:0] cmd_to_send,
 	// output to host
-	output reg ack_out,// acknowledge of package reception from host
-	output reg strobe_out, // states that a response has been received
-	output reg [39:0] response,
+	output wire ack_out,// acknowledge of package reception from host
+	output wire strobe_out, // states that a response has been received
+	output wire [39:0] response,
 	
 	//PAD_Pin
 	
@@ -56,7 +57,7 @@ serialToParallelWrapper # (48) stpw(
 .serial(padserial),
 .complete(reception_complete),.
 parallel(pad_response));
-
+///pad
 PAD command_PAD(
 .clock(sd_clock),
 .output_input(pad_state),
@@ -65,12 +66,27 @@ PAD command_PAD(
 .data_out(padserial),
 .io_port(cmd_pin)
 );
-
+//physical layer controller
 cmd_phys_controller cpc(
-
-
+.sd_clock(sd_clock),
+.reset(reset),
+.strobe_in(strobe_in),
+.ack_in(ack_in),
+.idle_in(idle_in),
+.cmd_to_send(cmd_to_send),
+.ack_out(ack_out),
+.strobe_out(strobe_out),
+.response(response),
+.pad_response(pad_response),
+.transmission_complete(transmission_complete),
+.reception_complete(reception_complete),
+.frame(frame),
+.reset_wrapper(reset_wrapper),
+.pad_state(pad_state),
+.pad_enable(pad_enable),
+.enable_pts_wrapper(enable_pts_wrapper),
+.enable_stp_wrapper(enable_stp_wrapper)
 );
-//enable to pad and wrapper parallel to serial must be at the same time as load 
-// enable to wrapper serial to paralel must be when send is completed at the start of waiting for response state.
 
-//logica de proximo estado
+
+endmodule
