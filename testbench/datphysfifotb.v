90b88dca3d5d997d0f5166421c20f70bdc2fc597
Diego Soto <ludisu13@gmail.com>
2016-06-18 02:33:19 -0600

    prueba physical  fifoi

--- testbench/datphysfifotb.v
+++ testbench/datphysfifotb.v
@@ -10,10 +10,12 @@
 `include "../code/dat_phys_controller.v"
 `include "../code/dat_phys.v"
 `include "../code/paralleltoserialwrapper_Sd.v"
+`include "../code/fifo2.v"
 
 
 module TestBench;
 wire pad;
+wire [31:0] dataFIFO;
 dat_phys dat(
 .sd_clock(sd_clock),
 .reset(reset),
@@ -23,10 +25,12 @@ dat_phys dat(
 .TIMEOUT_REG(16'd100),
 .blocks(4'd2),
 .writeRead(1'b0),
+.read_enable(write_enable),
 .multiple(1'b1),
 //.dat_pin(pad),
 .dat_pin(dat_to_card),
-.dataFROMFIFO(32'b0)
+.dataFROMFIFO(32'b0),
+.dataToFIFO(dataFIFO)
 );
 wire dat_to_card;
 generatorCMDcontroller gencmd(
@@ -34,12 +38,24 @@ generatorCMDcontroller gencmd(
 .reset(reset),
 .strobe_in(strobe_in)
 );
+
+	fifo #(32,8,3) fifo1  (
+		.write_clock(sd_clock),	//input
+		.read_clock(sd_clock), 	//input
+		.reset(reset), 				//input
+		.data(dataFIFO), 				//input 32
+		.write_enable(write_enable),//input
+		.read_enable(read_enable), 	//input
+	//	.q(q),  					//output 32
+		.fifo_full(fifo_full),  	//output
+		.fifo_empty(fifo_empty) 	//output
+	);
 reg Enable_card;
 reg load_send_card;
 
 wire [49:0] to_send;
 wire [48:0] to_send_kk;
-assign to_send_kk=48'd7924;
+assign to_send_kk=48'd79233334;
 assign to_send={1'b1,to_send_kk};
 
 paralleltoserialWrappersd # (51,8) sd(
@@ -53,10 +69,11 @@ paralleltoserialWrappersd # (51,8) sd(
 parallel({to_send,1'b1}));
 
 reg reset_card;
-
+reg read_enable;
 	initial begin
-	$dumpfile("dat_phys_2.vcd");
+	$dumpfile("dat_phys_fifo.vcd");
 		$dumpvars;	
+		read_enable=1'b0;
 		Enable_card=1'b0;
 		reset_card=1'b0;
 		load_send_card=1'b0;
@@ -86,6 +103,9 @@ reg reset_card;
 		#200;
 		reset_card=1'b0;
 		#200
+		read_enable=1'b1;
+		#40
+		read_enable=1'b0;
 		#2000
 		load_send_card=1'b1;
 		#10000
