b7791dadce1bdea19e85d705d2595598e7ef87f7
erickeduarte <erickedur@gmail.com>
2016-06-18 18:02:46 -0600

    registers test

---
+++ testbench/definitionsTestRegisters.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+`ifndef DEFINTIONS_V
+`define DEFINTIONS_V
+	
+`define CLOCK_HALF_PERIOD 20
+`define RESET_FIRE_TIME 60
+`define RESET_DEACTIVATE_TIME 60
+`define ADR_IN 40
+`define NOWRITE 120
+`define WRITE 840   
+`define NOREAD 1000
+`define READ 900
+`define NOCMD 1900
+`define CMD 100
+`define NEW_DATA 40
+`define NEW_RESPONSE 200
+`define NOERROR 100
+`define ERROR 1000
+`define NONORMAL 100
+`define NORMAL 1000
+
+`endif
